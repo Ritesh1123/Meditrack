3ef8589a82dddcaf9e7f2c749e9718ae81208aa7
Ritesh1123 <reeteshsharma889@gmail.com>
2025-08-25 13:45:29 +0530

    added auth controller

---
+++ controllers/authController.js
@@ -0,0 +1,30 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPassword, role });
+    res.json({ message: "User registered", user });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+    res.json({ token, user });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
